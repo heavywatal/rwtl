c0d75bb6db9c410cecaba0b5e0bd3c758e9ee069
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-06-16 17:37:56 +0900

    write_df() chooses compressor according to filename

--- R/write.R
+++ R/write.R
@@ -10,14 +10,21 @@ save_as = function(obj, dir='.') {
     cat('wrote: ', filepath, '\n')
 }
 
-#' Write gzipped file with write.table() and gzfile()
+#' Write commpressed file according to the filename
 #' @inheritParams readr::write_delim
 #' @inheritParams utils::write.table
-#' @param ... passed to gzfile()
+#' @param ... passed to `*zfile()`
 #' @rdname write
 #' @export
-write_gz_table = function(x, path, delim='\t', na='NA', append=FALSE, col_names=!append, ...) {
-    utils::write.table(x, gzfile(path, ...),
+write_df = function(x, path, delim='\t', na='NA', append=FALSE, col_names=!append, ...) {
+    file = if (grepl('gz$', path)) {
+        gzfile(path, ...)
+    } else if (grepl('bz2$', path)) {
+        bzfile(path, ...)
+    } else if (grepl('xz$', path)) {
+        xzfile(path, ...)
+    } else {path}
+    utils::write.table(x, file,
         append=append, quote=FALSE, sep=delim, na=na,
         row.names=FALSE, col.names=col_names)
 }
