45fcf21449c557116bd18dce2e188b31369dae84
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-07-17 23:30:36 +0900

    Add pythonic string functions

--- R/string.R
+++ R/string.R
@@ -5,7 +5,23 @@
 #' @return a list of character vectors
 #' @rdname string
 #' @export
-rsplit = function(string, pattern, n=42) {
+rsplit = function(string, pattern, n=42L) {
     pattern = sprintf('%s(?!(?:.*%s){%d})', pattern, pattern, n)
     stringr::str_split(string, pattern)
+}
+
+#' Pythonic alias of charToRaw()
+#' @param char character
+#' @rdname string
+#' @export
+ord = function(char) {
+    strtoi(charToRaw(char), 16L)
+}
+
+#' Pythonic alias of intToUtf8()
+#' @param i integer
+#' @rdname string
+#' @export
+chr = function(i) {
+    intToUtf8(i)
 }
