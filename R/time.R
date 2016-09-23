472163be53c3cc4c241b02178a517806e6cd2375
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-09-23 18:08:07 +0900

    Add time functions

---
+++ R/time.R
@@ -0,0 +1,15 @@
+#' Snippet for time in R
+#' @param format string
+#' @return string
+#' @rdname time
+#' @export
+today = function(format='%Y%m%d') {
+    base::format(Sys.time(), format)
+}
+
+#' @return string
+#' @rdname time
+#' @export
+now = function(format='%Y%m%d_%H%M%S') {
+    base::format(Sys.time(), format)
+}
