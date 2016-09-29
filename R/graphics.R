a0131b80e2ce6904521769f565ee9a59978d6a2c
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-09-29 18:11:22 +0900

    Add incomplete theme

--- R/graphics.R
+++ R/graphics.R
@@ -22,13 +22,38 @@ col2hex = function(color=grDevices::colors()) {
     grDevices::rgb(t(grDevices::col2rgb(color)), max=255)
 }
 
-#' Simple theme
+#' Incomplete theme that does not overwrite other elements
+#' @inheritParams ggplot2::ggtheme
+#' @rdname graphics
+#' @export
+theme_clean = function(base_size=12, base_family='sans') {
+    ggplot2::theme(
+        text= ggplot2::element_text(size=base_size, family=base_family),
+        panel.background= ggplot2::element_rect(fill=NA),
+        panel.border= ggplot2::element_rect(colour='grey50', fill=NA),
+        panel.grid.major= ggplot2::element_line(colour='grey90', size=0.2),
+        panel.grid.minor= ggplot2::element_blank(),
+        strip.background = ggplot2::element_rect(fill="grey90", colour="grey50", size=0.2),
+        legend.key= ggplot2::element_rect(fill=NA))
+}
+
+#' Complete theme
 #' @inheritParams ggplot2::ggtheme
 #' @rdname graphics
 #' @export
 theme_wtl = function(base_size=12, base_family='sans') {
-    ggplot2::theme_bw(base_size, base_family)+
-    ggplot2::theme(panel.grid.minor=ggplot2::element_blank())
+    ggplot2::theme_grey(base_size, base_family)+theme_clean(base_family)
+}
+
+#' Set L-shaped axes
+#' @inheritParams ggplot2::element_line
+#' @rdname graphics
+#' @export
+axis_line = function(colour=NULL, size=NULL, linetype=NULL, lineend=NULL) {
+    el = ggplot2::element_line(colour=colour, size=size, linetype=linetype, lineend=lineend)
+    ggplot2::theme(
+        panel.border= ggplot2::element_blank(),
+        axis.line.x= el, axis.line.y= el)  # workaround for ggplot2 2.1.0
 }
 
 #' Save PDF with quartz
