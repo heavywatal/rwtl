0dad4ec652694f75935cc41f8a490ec8cd667ccc
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-10-14 09:48:16 +0900

    Import fstats from tumorr

---
+++ R/fstats.R
@@ -0,0 +1,17 @@
+#' Calculate Kst by Hudson, Boos, and Kaplan (1992)
+#' @param within,between mean branch length or diversity
+#' @param n number of subpopulations
+#' @return numeric
+#' @rdname fstats
+#' @export
+fst_HBK = function(within, between, n=2) {
+    (between - within) / (between + within / (n - 1))
+}
+
+#' Calculate Fst by Hudson, Slatkin, and Maddison (1992)
+#' @return numeric
+#' @rdname fstats
+#' @export
+fst_HSM = function(within, between) {
+    1.0 - within / between
+}
