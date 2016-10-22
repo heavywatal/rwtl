0a8d51e819a6fbe4b5d6bf7d68b8d0be7113d30a
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-10-22 10:47:08 +0900

    import exec/rende.R from hg/rstats

---
+++ exec/rende.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+argv = commandArgs(trailingOnly=TRUE)
+infile = argv[1]
+outdir = argv[2]
+if (is.na(outdir)) {outdir = getwd()}
+message("infile: ", infile)
+message("outdir: ", outdir)
+rmarkdown::render(infile, output_dir=outdir)
