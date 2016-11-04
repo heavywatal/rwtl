a70c6e39cd537208fb977c80bf06404b4e96cf80
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-11-04 16:03:32 +0900

    map_par() accepts extra arguments

--- R/parallel.R
+++ R/parallel.R
@@ -11,13 +11,18 @@ map_par = function(.x, .f, ...,
     .cores=parallel::detectCores(logical=FALSE),
     .cluster=c('FORK', 'PSOCK')) {
 
-    .f = purrr::as_function(.f, ...)
+    if (is.function(.f)) {
+        .fun = purrr::partial(.f, ...)
+    } else {
+        .fun = purrr::as_function(.f, ...)
+    }
     .cores = min(.cores, length(.x))
     cluster = parallel::makeCluster(.cores, match.arg(.cluster), outfile='')
     on.exit(parallel::stopCluster(cluster))
     doParallel::registerDoParallel(cluster)
-    foreach::foreach(args=.x, .combine=.combine, .multicombine=.multicombine, .inorder=.inorder, .packages=.packages) %dopar% {
-        .f(args)
+    x = NULL # to suppress warning
+    foreach::foreach(x=.x, .combine=.combine, .multicombine=.multicombine, .inorder=.inorder, .packages=.packages) %dopar% {
+        .fun(x)
     }
 }
 
