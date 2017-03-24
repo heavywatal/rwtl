77e9871de9ed38b831b5684ee18e8b92746e1c61
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-24 13:57:53 +0900

    Remove write_df() because readr 1.1.0 supports compressed file transparently

--- R/write.R
+++ R/write.R
@@ -9,22 +9,3 @@ save_as = function(obj, dir='.') {
     saveRDS(obj, file=filepath)
     cat('wrote: ', filepath, '\n')
 }
-
-#' Write commpressed file according to the filename
-#' @inheritParams readr::write_delim
-#' @inheritParams utils::write.table
-#' @param ... passed to `*zfile()`
-#' @rdname write
-#' @export
-write_df = function(x, path, delim='\t', na='NA', append=FALSE, col_names=!append, ...) {
-    file = if (grepl('gz$', path)) {
-        gzfile(path, ...)
-    } else if (grepl('bz2$', path)) {
-        bzfile(path, ...)
-    } else if (grepl('xz$', path)) {
-        xzfile(path, ...)
-    } else {path}
-    utils::write.table(x, file,
-        append=append, quote=FALSE, sep=delim, na=na,
-        row.names=FALSE, col.names=col_names)
-}
