f5de1594ec6e35ec07e26ffe191a00883984a88b
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-06-26 23:57:41 +0900

    Add padding option to logo

--- demo/logo.R
+++ demo/logo.R
@@ -1,16 +1,17 @@
 library(tidyverse)
 
-plot_logo = function(circle_scale=1.0) {
+plot_logo = function(circle_scale=1.0, padding=10) {
     .path = tibble(x=c(35, 11, 55, 41, 75, 71, 95),
                    y=c(95,  5, 73,  5, 51,  5, 29))
     .poly = tibble(x=c( 1,  6, 99),
                    y=c(35, 22, 72))
     .dot = tibble(x=77, y=77)
+    .limits = c(-padding, 100 + padding)
     ggplot(.path, aes(x, y))+
     geom_polygon(data=.poly, fill='#e08010')+
     geom_path(size=6, linejoin='mitre')+
     geom_point(size=18 * circle_scale, data=.dot, colour='#a4321a')+
-    coord_fixed(xlim=c(0, 100), ylim=c(0, 100), expand=FALSE)+
+    coord_fixed(xlim=.limits, ylim=.limits, expand=FALSE)+
     theme_void()
 }
 
