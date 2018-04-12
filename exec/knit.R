32677ffa5324d32d3689ccf49a97b47a49725177
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-04-12 19:11:50 +0900

    :sparkles: Pass second argument to knitr::knit()

--- exec/knit.R
+++ exec/knit.R
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript --no-init-file
 argv = commandArgs(trailingOnly = TRUE)
-stopifnot(length(argv) > 0)
-knitr::knit(argv[1])
+stopifnot(length(argv) > 0L)
+knitr::knit(argv[1L], argv[2L])
