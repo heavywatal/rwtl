4e7083dc8d11b7348eb017cefae7282aee8afd27
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-04-15 22:31:54 +0900

    :sparkles: Override force_assignment_op() and use it in styler_style()

--- R/style.R
+++ R/style.R
@@ -1,17 +1,41 @@
-#' tidyverse_style but use equal sign = for assignment
-#' @inheritParams styler::tidyverse_style
+#' Apply tidyverse_style, but use equal sign = for assignment
 #' @rdname style
 #' @export
-eq_assign_style = function(scope="tokens", strict=TRUE, indent_by=2,
-                           start_comments_with_one_space=FALSE,
-                           reindention=styler::tidyverse_reindention(),
-                           math_token_spacing=styler::tidyverse_math_token_spacing()) {
-  x = styler::tidyverse_style(
-    scope = scope, strict = strict, indent_by = indent_by,
-    start_comments_with_one_space = start_comments_with_one_space,
-    reindention = reindention,
-    math_token_spacing = math_token_spacing
-  )
-  x$token$force_assignment_op = NULL
+eq_assign_style = function(...) {
+  x = styler::tidyverse_style(...)
+  x$token$force_assignment_op = function(pd) {
+    to_replace = (pd$token == "LEFT_ASSIGN" & pd$text == "<-")
+    pd$token[to_replace] = "EQ_ASSIGN"
+    pd$text[to_replace] = "="
+    pd
+  }
   x
+}
+
+#' @inheritParams styler::style_pkg
+#' @inheritParams styler::style_dir
+#' @rdname style
+#' @export
+styler_style = function(path=".", ..., style = eq_assign_style, filetype = "R",
+                        recursive = TRUE, exclude_files = NULL) {
+  if (fs::is_dir(path)) {
+    .pkg_files = c("DESCRIPTION", "NAMESPACE", "R")
+    if (all(fs::file_exists(fs::path(path, .pkg_files)))) {
+      styler::style_pkg(
+        path, ..., style = style, filetype = filetype,
+        exclude_files = c(exclude_files, "R/RcppExports.R")
+      )
+    } else {
+      styler::style_dir(
+        path, ..., style = style, filetype = filetype, recursive = recursive,
+        exclude_files = exclude_files
+      )
+    }
+  } else {
+    if (fs::file_exists(path)) {
+      styler::style_file(path, ..., style = style)
+    } else {
+      styler::style_file(path, ..., style = style)
+    }
+  }
 }
