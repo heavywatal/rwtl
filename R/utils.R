cb8d8501e26566eb233e5169a429132c3f4c1f69
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-04-30 16:57:45 +0900

    :wrench: Override print.tbl_df in max_print() and less()

--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,17 @@ easierprof = function(expr, interval=0.02, memory=FALSE) {
   utils::summaryRprof(.tmpfile, memory = ifelse(memory, "both", "none"))
 }
 
+print_options = function(height, width) {
+  options(
+    datatable.print.nrows = height,
+    datatable.print.topn = height %/% 2L,
+    tibble.print_max = height,
+    tibble.print_min = height,
+    tibble.width = width,
+    width = min(width, 10000L)
+  )
+}
+
 #' Set width and height according to the current environment
 #' @param max.print maximum number of rows to print
 #' @rdname utils
@@ -22,13 +33,9 @@ adjust_print_options = function(max.print = 30L) {
   stty_size = system("stty size", intern = TRUE)
   stopifnot(length(stty_size) > 0L)
   stty_size = strsplit(stty_size, " ")[[1L]]
-  tibble_height = min(as.integer(stty_size[1L]) - 6L, max.print)
-  options(
-    width = as.integer(stty_size[2L]),
-    datatable.print.nrows = tibble_height,
-    datatable.print.topn = tibble_height %/% 2L,
-    tibble.print_max = tibble_height,
-    tibble.print_min = tibble_height
+  print_options(
+    height = min(as.integer(stty_size[1L]) - 6L, max.print),
+    width = as.integer(stty_size[2L])
   )
   if (interactive()) {
     message("width: ", getOption("width"))
@@ -39,35 +46,28 @@ adjust_print_options = function(max.print = 30L) {
   }
 }
 
+#' Print big tibble as it is
+#' @inheritParams base::print
+#' @rdname utils
+#' @export
+max_print = function(x, max.print=getOption("max.print"), width=Inf, ...) {
+  opts = print_options(width, max.print)
+  on.exit(options(opts))
+  print.tbl_df = data.table:::print.data.table
+  print.tbl = data.table:::print.data.table
+  print(x, ...)
+}
+
 #' Shortcut of page(x, method='print')
-#' @inheritParams utils::page
 #' @param width integer
 #' @rdname utils
 #' @export
-less = function(x, method=c("print", "dput"),
-                max.print=getOption("max.print"),
-                width=getOption("width"), ...) {
-  opts = options(
-    max.print = max.print,
-    tibble.print_max = max.print,
-    tibble.print_min = max.print,
-    width = width
-  )
-  on.exit(options(opts))
-  utils::page(x, match.arg(method), ...)
-}
-
-#' Print big tibble as it is
-#' @rdname utils
-#' @export
-max_print = function(x, max.print=getOption("max.print"), ...) {
-  opts = options(
-    max.print = max.print,
-    tibble.print_max = max.print,
-    tibble.print_min = max.print
-  )
-  on.exit(options(opts))
-  print(x, ...)
+less = function(x, max.print=getOption("max.print"), width=Inf, ...) {
+  file = tempfile("Rpage.")
+  sink(file)
+  max_print(x, max.print = max.print, width = width, ...)
+  sink()
+  file.show(file, delete.file = TRUE)
 }
 
 #' Workaround to achieve devtools::document(..., export_all=FALSE)
