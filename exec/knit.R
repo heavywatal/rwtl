be9a2106a8abb18a12f8d948b58a6beddae15170
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-05-09 01:27:30 +0900

    :art: Set fig.path if output=argv[2] is provided

--- exec/knit.R
+++ exec/knit.R
@@ -1,4 +1,9 @@
 #!/usr/bin/env Rscript --no-init-file
 argv = commandArgs(trailingOnly = TRUE)
 stopifnot(length(argv) > 0L)
-knitr::knit(argv[1L], argv[2L])
+output = argv[2L]
+if (!is.na(output)) {
+  fig.path = paste0(dirname(output), "/figure/")
+  knitr::opts_chunk$set(fig.path = fig.path)
+}
+knitr::knit(argv[1L], output)
