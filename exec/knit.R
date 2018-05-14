9bf772fe2e331498c386582c0da42753cc177046
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-05-14 13:04:29 +0900

    :wrench: setwd(dirname(input)) before knit

--- exec/knit.R
+++ exec/knit.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript --no-init-file
 argv = commandArgs(trailingOnly = TRUE)
 stopifnot(length(argv) > 0L)
+input = argv[1L]
 output = argv[2L]
 if (!is.na(output)) {
-  outdir = dirname(normalizePath(output))
-  knitr::opts_knit$set(base.dir = outdir)
+  output = normalizePath(output)
+  knitr::opts_knit$set(base.dir = dirname(output))
 }
-knitr::knit(argv[1L], output)
+setwd(dirname(input))
+knitr::knit(basename(input), output)
