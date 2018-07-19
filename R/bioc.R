3deecbca734f5dac2cd5a0fc8184de7300da5406
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-07-19 22:16:23 +0900

    :art: Simplify library_bioc()

--- R/bioc.R
+++ R/bioc.R
@@ -1,9 +1,9 @@
-#' source('*/biocLite.R') and biocLite()
+#' Shortcut for `source()` and `biocLite()`
 #' @param mirror URL for BioC_mirror
-#' @param ask passed to biocLite()
+#' @param ... passed to `biocLite()`
 #' @rdname bioc
 #' @export
-library_bioc = function(mirror=getOption("BioC_mirror"), ask=FALSE) {
+library_bioc = function(mirror=getOption("BioC_mirror", "https://bioconductor.org"), ...) {
   source(sprintf("%s/biocLite.R", mirror))
-  BiocInstaller::biocLite(ask = ask)
+  BiocInstaller::biocLite(...)
 }
