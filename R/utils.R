a97de32cbe0d46570082b9981b44031f90f11105
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-22 19:12:25 +0900

    :sparkles: Create getenv()

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,18 @@ easierprof = function(expr, interval=0.02, memory=FALSE) {
   utils::Rprof(NULL)
   utils::summaryRprof(.tmpfile, memory = ifelse(memory, "both", "none"))
 }
+
+#' @description
+#' `getenv` converts a result of `Sys.getenv()` to data.frame.
+#' @param pattern R expression
+#' @rdname utils
+#' @export
+getenv = function(pattern = NULL) {
+  env = Sys.getenv()
+  class(env) = "character"
+  env = tibble::tibble(key = names(env), value = env)
+  if (!is.null(pattern)) {
+    env = dplyr::filter(env, stringr::str_detect(.data$key, pattern))
+  }
+  env
+}
