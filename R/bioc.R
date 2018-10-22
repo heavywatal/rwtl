4c9faaa99f89da36f54a4112825f72052a4f97a7
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-23 00:49:53 +0900

    :fire: Remove library_bioc() in favor of BiocManager::install()

--- R/bioc.R
+++
@@ -1,9 +0,0 @@
-#' Shortcut for `source()` and `biocLite()`
-#' @param mirror URL for BioC_mirror
-#' @param ... passed to `biocLite()`
-#' @rdname bioc
-#' @export
-library_bioc = function(mirror=getOption("BioC_mirror", "https://bioconductor.org"), ...) {
-  source(sprintf("%s/biocLite.R", mirror))
-  BiocInstaller::biocLite(...)
-}
