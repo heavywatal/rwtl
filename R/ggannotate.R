027a0ed91dc275f03f6c3370f053cd0c3e4f781d
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-09 18:10:07 +0900

    :sparkles: Add annotate_polygon2() to control linejoin

--- R/ggannotate.R
+++ R/ggannotate.R
@@ -1,17 +1,32 @@
 #' Variations of `ggplot2::annotate()`
 #'
 #' @description
-#' `annotate_polygon()` is a shortcut of `ggplot2::annotate("polygon")`
+#' `annotate_polygon()` is a shortcut of `ggplot2::annotate("polygon")`.
 #' @param x,y center coordinate
+#' @param n number of sides
 #' @param radius size of polygon
 #' @param stroke line width
-#' @param n number of sides
-#' @param ... passed to `ggplot2::annotate()`
+#' @param ... passed to `ggplot2::annotate("polygon")`
 #' @rdname ggannotate
 #' @export
-annotate_polygon = function(x, y, radius = 1, stroke = 1, n = 6L, ...) {
-  angle = seq_len(n + 1L) * 2 * pi / n + pi / 2
+annotate_polygon = function(x, y, n = 6L, radius = 1, stroke = 1, ...) {
+  angle = seq_len(n) * 2 * pi / n + pi / 2
   x = x + radius * cos(angle)
   y = y + radius * sin(angle)
   ggplot2::annotate("polygon", x = x, y = y, size = stroke, ...)
+}
+
+#' @description
+#' `annotate_polygon2()` has more control on line stroke.
+#' @param colour,linejoin passed to `ggplot2::annotate("path")`
+#' @rdname ggannotate
+#' @export
+annotate_polygon2 = function(x, y, n = 6L, radius = 1, stroke = 1, colour = "#666666", linejoin = "mitre", ...) {
+  angle = seq_len(n + 2L) * 2 * pi / n + pi / 2
+  x = x + radius * cos(angle)
+  y = y + radius * sin(angle)
+  list(
+    ggplot2::annotate("polygon", x = x, y = y, ...),
+    ggplot2::annotate("path", x = x, y = y, size = stroke, colour = colour, linejoin = linejoin)
+  )
 }
