e09d441a50787863efe3ac94b170c5f07e8074be
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-09 17:32:53 +0900

    :art: Remove warnings

--- R/graphics.R
+++ R/graphics.R
@@ -6,12 +6,12 @@
 pch_plot = function() {
   df = tibble::tibble(
     pch = seq_len(128L) - 1L,
-    x = pch %% 16L,
-    y = pch %/% 16L
+    x = .data$pch %% 16L,
+    y = .data$pch %/% 16L
   )
-  ggplot2::ggplot(df, ggplot2::aes(x, y)) +
-    ggplot2::geom_text(ggplot2::aes(label = pch), size = 4, position = ggplot2::position_nudge(0, 0.3)) +
-    ggplot2::geom_point(ggplot2::aes(shape = pch), size = 5, stroke = 2, fill = "tomato") +
+  ggplot2::ggplot(df, ggplot2::aes_(~x, ~y)) +
+    ggplot2::geom_text(ggplot2::aes_(label = ~pch), size = 4, position = ggplot2::position_nudge(0, 0.3)) +
+    ggplot2::geom_point(ggplot2::aes_(shape = ~pch), size = 5, stroke = 2, fill = "tomato") +
     ggplot2::scale_shape_identity() +
     ggplot2::scale_y_reverse() +
     ggplot2::theme_void()
