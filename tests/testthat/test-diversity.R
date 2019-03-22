17385a3ec4542ddad44d0d41de897bbf4e08e9e6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-22 22:23:30 +0900

    :white_check_mark: Add tests for diversity.R

--- tests/testthat/test-diversity.R
+++ tests/testthat/test-diversity.R
@@ -1,10 +1,17 @@
 context("test-diversity.R")
 
-test_that("shannon_index", {
+test_that("shannon_index works", {
+  expect_equal(shannon_index(numeric(0L)), 0)
   expect_equal(shannon_index(c(1)), 0)
 })
 
-test_that("simpson_index", {
+test_that("simpson_index works", {
+  expect_equal(simpson_index(numeric(0L)), 0)
   expect_equal(simpson_index(c(1)), 1)
   expect_equal(simpson_index(c(1, 1)), 0.5)
+})
+
+test_that("evenness works", {
+  expect_equal(evenness(numeric(0L)), 0)
+  expect_equal(evenness(c(1)), NaN)
 })
