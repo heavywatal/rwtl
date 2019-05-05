22213e6711144be4bf96a0656af7798b0a0a9c5e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-05-05 09:28:57 +0900

    :white_check_mark: Add tests for mcmap

---
+++ tests/testthat/test-parallel.R
@@ -0,0 +1,8 @@
+test_that("mcmap works", {
+  expect_identical(mcmap(1:2, identity), list(1L, 2L))
+  expect_identical(mcmap_lgl(1:2, ~ .x > 1L), c(FALSE, TRUE))
+  expect_identical(mcmap_int(1:2, identity), c(1L, 2L))
+  expect_identical(mcmap_dbl(1:2, as.double), c(1, 2))
+  expect_identical(mcmap_chr(1:2, as.character), c("1", "2"))
+  expect_identical(mcmap_dfr(1:2, ~ iris[.x,]), utils::head(iris, 2L))
+})
