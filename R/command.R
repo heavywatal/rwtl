760f1ae0567f8456402c49abf89a97ffbaf3192c
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-05-13 18:57:03 +0900

    :fire: Remove this_file() in favor of command_args()$file

--- R/command.R
+++ R/command.R
@@ -5,20 +5,10 @@
 #' @rdname command
 #' @export
 command_args = function() {
-  .argv = commandArgs(trailingOnly = FALSE)
-  l = list()
-  l$file = sub("^--file=", "", grep("^--file=", .argv, value = TRUE))
-  l$srcdir = dirname(normalizePath(l$file))
-  l$args = grep("^[^-]", .argv[-1], value = TRUE)
-  return(l)
-}
-
-#' @details
-#' `this_file` extracts script path from `commandArgs()`.
-#' @rdname command
-#' @export
-this_file = function() {
-  args = commandArgs(trailingOnly = FALSE)
-  file_arg = grep("--file=", args, value = TRUE)
-  stringr::str_replace(file_arg, "^--file=", "")
+  argv = commandArgs(trailingOnly = FALSE)
+  x = list()
+  x$file = sub("^--file=", "", grep("^--file=", argv, value = TRUE))
+  x$srcdir = dirname(normalizePath(x$file))
+  x$args = grep("^[^-]", argv[-1], value = TRUE)
+  x
 }
