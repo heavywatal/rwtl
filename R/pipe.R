4a9709876ccffa644b5f532110d7052ec96a836a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-05-17 11:26:27 +0900

    :white_check_mark: Enable examples; rename grepp and %|%

--- R/pipe.R
+++ R/pipe.R
@@ -1,24 +1,23 @@
 #' Pipe text to system commands
 #'
 #' @examples
-#' \dontrun{
-#' mpg %>% less()
-#' mpg %>% grepp(manual)
-#' mpg %>% grepp("manual")
-#' mpg %>% pipeshell(wc)
+#' `%>%` = magrittr::`%>%`
+#' iris %>% less()
+#' iris %>% egrep(vir)
+#' iris %>% egrep("vir")
+#' iris %>% pipeshell(wc)
 #'
-#' iris %|% wc
-#' iris %|% "wc -l"
-#' iris %|% `grep seto | less`
+#' iris %P% wc
+#' iris %P% "wc -l"
+#' iris %P% `grep seto | less`
 #'
 #' vir = "seto"
 #' iris %G% vir
 #' iris %G% !!vir
 #' iris %G% `-v seto | less`
-#' }
 #' @inheritParams max_print
 #' @details
-#' `pipeshell` and `%|%` send `x` to `command` via shell.
+#' `pipeshell` and `%P%` send `x` to `command` via shell.
 #' @param command to which the text is sent; string or expression.
 #' @rdname pipe
 #' @export
@@ -38,7 +37,7 @@ pipeshell.data.frame = function(x, command, ..., n = getOption("max.print"), wid
 
 #' @rdname pipe
 #' @export
-`%|%` = function(x, command) {
+`%P%` = function(x, command) {
   pipeshell(x, !!rlang::enquo(command))
 }
 
@@ -52,20 +51,20 @@ less = function(x, ...) {
 }
 
 #' @details
-#' `grepp` and `%G%` are shorthand for `x %|% "grep -P {expr}"`.
-#' @param expr arguments to `grep -P`; string or expression.
+#' `egrep` and `%G%` are shorthand for `x %P% "egrep {expr}"`.
+#' @param expr arguments to `egrep`; string or expression.
 #' @rdname pipe
 #' @export
-grepp = function(x, expr, ...) {
+egrep = function(x, expr, ...) {
   expr = rlang::as_name(rlang::enquo(expr))
-  command = paste("grep -P", expr)
+  command = paste("egrep", expr)
   pipeshell(x, command = !!command, ...)
 }
 
 #' @rdname pipe
 #' @export
 `%G%` = function(x, expr) {
-  grepp(x, !!rlang::enquo(expr))
+  egrep(x, !!rlang::enquo(expr))
 }
 
 sinkpipe = function(expr, command) {
