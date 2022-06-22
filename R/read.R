32d2293221d6bd0d4521bf77dcb6198d8b54b7ad
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-06-22 20:56:15 +0900

    :art: Use I() to follow readr/vroom updates

--- R/read.R
+++ R/read.R
@@ -1,19 +1,19 @@
 #' Functions to read files
 #'
 #' @details
-#' `read_boost_ini` reads an INI-like config file of boost::program_options.
+#' `read_boost_ini()` reads an INI-like config file of boost::program_options.
 #' @param file filename or text
 #' @rdname read
 #' @export
 read_boost_ini = function(file) {
-  readr::read_delim(file, "=", col_names = c("key", "val"), comment = "#", trim_ws = TRUE) |>
+  tsv = readr::read_delim(file, "=", col_names = c("key", "val"), comment = "#", trim_ws = TRUE) |>
     dplyr::summarize(dplyr::across(tidyselect::everything(), paste0, collapse = "\t")) |>
-    paste0(collapse = "\n") |>
-    readr::read_tsv()
+    paste0(collapse = "\n")
+  readr::read_tsv(I(tsv))
 }
 
 #' @details
-#' `read_pb` reads paste board into data.frame.
+#' `read_pb()` reads paste board into data.frame.
 #' @rdname read
 #' @export
 read_pb = function() {
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,5 +1,5 @@
 test_that("read_boost_ini works", {
   ini = "x = 42\ny = text\n"
   tsv = "x\ty\n42\ttext\n"
-  expect_equal(read_boost_ini(ini), readr::read_tsv(tsv))
+  expect_identical(read_boost_ini(I(ini)), readr::read_tsv(I(tsv)))
 })
