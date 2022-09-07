dc4f1d9e4a622e7396cac18f422e4e08a7f0b0ae
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-09-07 20:48:08 +0900

    :art: Separate rle_df() and rle_ranges()
    rle_ranges() and table_df() receive dots for naming.

--- R/tibbled.R
+++ R/tibbled.R
@@ -1,26 +1,36 @@
 #' Tibbled version of base R functions
-
-#' @param x atomic vector
-#' @param name column name in output
+#'
+#' `rle_df()` returns a tibbled rle.
+#' @param x,... Atomic vector
 #' @seealso [rle()]
 #' @rdname tibbled
 #' @export
-rle_df = function(x, name = "value") {
-  x = rle(x)
-  tibble::tibble(
-    !!name := x[["values"]],
-    start = 0L,
-    end = cumsum(x[["lengths"]])
-  ) |>
-    dplyr::mutate(start = dplyr::lag(.data[["end"]], 1L, 0L) + 1L)
+rle_df = function(x) {
+  tibble::new_tibble(rle(x))
 }
 
+#' @description `rle_ranges()` returns an IRanges-like tibble.
+#' @rdname tibbled
+#' @export
+rle_ranges = function(...) {
+  qx = rlang::enquos(..., .named = TRUE)
+  name = names(qx)[[1L]]
+  x = rle(c(..., use.names = FALSE))
+  end = cumsum(x[["lengths"]])
+  start = c(0L, end[-length(end)]) + 1L
+  tibble::new_tibble(tibble::lst(start, end, {{ name }} := x[["values"]]))
+}
+
+#' @description `table_df()` returns a tibbled table.
+#' @param name The name of the new column in the output.
 #' @seealso [table()], [dplyr::count()]
 #' @rdname tibbled
 #' @export
-table_df = function(x, name = "n") {
-  qx = rlang::enquo(x)
+table_df = function(..., name = "n") {
+  qx = rlang::enquos(..., .named = TRUE)
+  x = rlang::eval_tidy(qx[[1L]])
   tab = table(x)
-  tibble::lst(!!qx := as.vector(names(tab), mode = mode(x)), !!name := as.integer(tab)) |>
-    tibble::new_tibble(nrow = length(tab))
+  lhs = names(qx)[[1L]]
+  rhs = as.vector(names(tab), mode = mode(x))
+  tibble::new_tibble(tibble::lst(!!lhs := rhs, !!name := as.integer(tab)))
 }
--- tests/testthat/test-tibbled.R
+++ tests/testthat/test-tibbled.R
@@ -1,18 +1,23 @@
 test_that("rle_df works", {
   v = c(0, 0, 0, 1, 1, 2)
-  expected = tibble::tibble(
-    value = c(0, 1, 2),
-    start = c(1L, 4L, 6L),
-    end = c(3L, 5L, 6L)
-  )
+  expected = tibble::tibble(lengths = c(3L, 2L, 1L), values = c(0, 1, 2))
   expect_identical(rle_df(v), expected)
+})
+
+test_that("rle_ranges works", {
+  v = c(0, 0, 0, 1, 1, 2)
+  expected = tibble::tibble(
+    start = c(1L, 4L, 6L),
+    end = c(3L, 5L, 6L),
+    v = c(0, 1, 2)
+  )
+  expect_identical(rle_ranges(v), expected)
+  expect_identical(rle_ranges(value = v), dplyr::rename(expected, value = v))
 })
 
 test_that("table_df works", {
   v = c(0, 0, 0, 1, 1, 2)
-  expected = tibble::tibble(
-    v = c(0, 1, 2),
-    n = c(3L, 2L, 1L)
-  )
+  expected = tibble::tibble(v = c(0, 1, 2), n = c(3L, 2L, 1L))
   expect_identical(table_df(v), expected)
+  expect_identical(table_df(value = v), dplyr::rename(expected, value = v))
 })
