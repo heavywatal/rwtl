751f8c90f8032603c4acf5ca9d914b673884a5a2
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-02-26 01:39:39 +0900

    :white_check_mark: Update function names in tests

--- tests/testthat/test-time.R
+++ tests/testthat/test-time.R
@@ -1,4 +1,4 @@
 test_that("time functions work", {
-  expect_match(today(), "^2\\d{7}$")
-  expect_match(now(), paste0(today(), "_\\d{6}$"))
+  expect_match(str_today(), "^2\\d{7}$")
+  expect_match(str_now(), paste0(str_today(), "_\\d{6}$"))
 })
