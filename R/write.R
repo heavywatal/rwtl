8b03d68afe190ef95b91b89706cfe39935e46601
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-03-16 01:44:40 +0900

    :sparkles: Create read_table()

--- R/write.R
+++ R/write.R
@@ -1,6 +1,8 @@
-#' Save R object with its name
-#' @param obj R object
-#' @param dir a string
+#' Write R objects to files
+#'
+#' `save_as()` saves R object with its name.
+#' @param obj An R object.
+#' @param dir A string.
 #' @rdname write
 #' @export
 save_as = function(obj, dir = ".") {
@@ -8,4 +10,14 @@ save_as = function(obj, dir = ".") {
   filepath = file.path(dir, paste0(obj_name, ".rds"))
   saveRDS(obj, file = filepath)
   cat("wrote: ", filepath, "\n")
+}
+
+#' @description
+#' `write_table()` is a shortcut of [readr::write_delim()] with decent default options.
+#' @inheritParams readr::write_delim
+#' @inheritDotParams readr::write_delim
+#' @rdname write
+#' @export
+write_table = function(x, file, delim = "\t", na = "", quote = "needed", ...) {
+  readr::write_delim(x, file, delim = delim, na = na, quote = quote, ...)
 }
