61093e89eda1e79c9ab3681494b7337bb693eeab
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-03-16 00:29:29 +0900

    :sparkles: Create gs4_read_sheet()

---
+++ R/drive.R
@@ -0,0 +1,31 @@
+#' Read spreadsheet from cloud drives
+#'
+#' @inheritDotParams googlesheets4::read_sheet
+#' @rdname drive
+#' @export
+gs4_read_sheet = function(..., path = NULL, email = gargle::gargle_oauth_email()) {
+  if (is.null(googledrive::drive_oauth_app())) {
+    gs4_auth_json(path, email)
+  }
+  googlesheets4::read_sheet(...)
+}
+
+#' @param path Credentials JSON downloaded from "Google Cloud Console".
+#' Passed to [googledrive::drive_auth_configure()].
+#' @param email An address or a glob pattern like `"*@tohoku.ac.jp"`.
+#' `TRUE` to allow auto-discovery from cache.
+#' Passed to [googledrive::drive_auth()]
+#' @rdname drive
+#' @export
+gs4_auth_json = function(path = NULL, email = gargle::gargle_oauth_email()) {
+  drive_auth_json(path, email)
+  googlesheets4::gs4_auth(token = googledrive::drive_token())
+}
+
+drive_auth_json = function(path, email) {
+  if (is.null(path)) {
+    path = Sys.getenv("GOOGLE_OAUTH_JSON")
+  }
+  googledrive::drive_auth_configure(path = path)
+  googledrive::drive_auth(email = email)
+}
