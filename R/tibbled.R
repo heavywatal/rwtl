42bfd7b22ec5cffecb7058e0ed2c9a5ba6cc94bc
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-09-08 21:55:24 +0900

    :sparkles: Create tabulate0() for counting integers

--- R/tibbled.R
+++ R/tibbled.R
@@ -34,3 +34,25 @@ table_df = function(..., name = "n") {
   rhs = as.vector(names(tab), mode = typeof(x))
   tibble::new_tibble(tibble::lst(!!lhs := rhs, !!name := as.integer(tab)))
 }
+
+#' @description `tabulate0_df()` is a tabulated variant of `table_df()`.
+#' @seealso [tabulate()]
+#' @rdname tibbled
+#' @export
+tabulate0_df = function(..., name = "n") {
+  qx = rlang::enquos(..., .named = TRUE)
+  x = rlang::eval_tidy(qx[[1L]])
+  tab = tabulate0(x)
+  lhs = names(qx)[[1L]]
+  rhs = seq.int(0L, max(x, na.rm = TRUE))
+  tibble::new_tibble(tibble::lst(!!lhs := rhs, !!name := as.integer(tab)))
+}
+
+#' @description
+#' `tabulate0()` is a variant of [tabulate()] to count integers including zero.
+#' @inheritParams base::tabulate
+#' @rdname tibbled
+#' @export
+tabulate0 = function(bin, nbins = max(1L, bin, na.rm = TRUE)) {
+  tabulate(bin + 1L, nbins = nbins + 1L)
+}
