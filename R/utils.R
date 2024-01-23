d7bdfaf89787dcbd849e5be241f06bd311a245a5
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-01-23 13:33:49 +0900

    :zap: Filter before creating tibble

--- R/utils.R
+++ R/utils.R
@@ -23,11 +23,13 @@ easierprof = function(expr, interval = 0.02, memory = FALSE) {
 getenv = function(pattern = NULL) {
   env = Sys.getenv()
   class(env) = "character"
-  env = tibble::tibble(key = names(env), value = env)
-  if (!is.null(pattern)) {
-    env = dplyr::filter(env, stringr::str_detect(.data$key, pattern))
+  key = names(env)
+  if (is.null(pattern)) {
+    tibble::tibble(key, value = env)
+  } else {
+    idx = stringr::str_detect(key, pattern)
+    tibble::tibble(key = key[idx], value = env[idx])
   }
-  env
 }
 
 #' @description
