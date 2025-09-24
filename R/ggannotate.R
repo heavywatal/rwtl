746ce126ea26df46e20ca08af55f8a215275f46a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-09-24 18:16:00 +0900

    feat!: Follow ggplot2 4.0.0

--- R/ggannotate.R
+++ R/ggannotate.R
@@ -3,24 +3,22 @@
 #' @description
 #' `annotate_regpolygon()` is a shortcut of `ggplot2::annotate("polygon")`
 #' to draw a regular polygon.
-#' @param n number of sides
-#' @param radius size of polygon
-#' @param x,y center coordinate
-#' @param start angle of the starting point
-#' @param linewidth,color passed to `ggplot2::annotate("path")`
-#' @param fill,... passed to `ggplot2::annotate("polygon")`
+#' @param n The number of sides.
+#' @param radius The distance from the center to a vertex.
+#' @param x,y The center coordinates.
+#' @param start The angle of the starting point.
+#' @param linejoin,... Extra arguments passed to `ggplot2::annotate("polygon")`.
 #' @examples
 #' ggplot2::ggplot() +
 #'   annotate_regpolygon(6L, radius = 5, linewidth = 6, color = "#333333", fill = "#ffffff") +
-#'   annotate_regpolygon(6L, radius = 2, fill = "#C41A41") +
+#'   annotate_regpolygon(6L, radius = 2, fill = "#C41A41", linetype = 0, linewidth = 0) +
 #'   ggplot2::coord_fixed()
 #' @rdname ggannotate
 #' @export
-annotate_regpolygon = function(n, radius = 1, x = 0, y = 0, start = pi / 2,
-                               linewidth = 0, color = "#333333", fill = "#666666", ...) {
+annotate_regpolygon = function(n, radius = 1, x = 0, y = 0, start = pi / 2, linejoin = "mitre", ...) {
   .df = make_regpolygon(n, radius, x, y, start)
   ggplot2::annotate("polygon",
-    x = .df[["x"]], y = .df[["y"]], linewidth = linewidth, color = color, fill = fill, ..., linejoin = "mitre"
+    x = .df[["x"]], y = .df[["y"]], linejoin = linejoin, ...
   )
 }
 
--- R/ggtheme.R
+++ R/ggtheme.R
@@ -1,31 +1,85 @@
 #' Shortcut for ggplot theme
 #'
 #' @description
-#' `theme_wtl()` is a slightly modified [ggplot2::theme_bw].
+#' `theme_wtl()` is a slightly modified variant of [ggplot2::theme_bw()].
+#' - Increase `base_size` default to 12.
+#' - Increase the small text size from `rel(0.8)` to `rel(1.0/1.2)`.
+#' - Increase the color contrast of `axis.text`.
+#' - Decrease the color contrast of `axis.ticks`.
+#' - Remove `panel.grid.minor`.
+#' - Remove `legend.key` background.
+#' - Set default color palettes to `viridis` and `Okabe-Ito`.
 #' @inheritParams ggplot2::theme_bw
+#' @examples
+#' p = ggplot2::ggplot(ggplot2::mpg) +
+#'   ggplot2::aes(displ, hwy, color = displ, fill = class) +
+#'   ggplot2::geom_point(shape = 21, size = 3, stroke = 2)
+#'
+#' p + ggplot2::theme_bw()
+#' p + theme_wtl()
+#' p + theme_wtl() + erase(axis.ticks) + axis_line()
 #' @rdname ggtheme
 #' @export
-theme_wtl = function(base_size = 12, base_family = "",
-                     base_line_size = base_size / 22, base_rect_size = base_size / 22) {
+theme_wtl = function(base_size = 12, base_family = "", header_family = NULL,
+                     base_line_size = base_size / 22, base_rect_size = base_size / 22,
+                     ink = "black", paper = "white", accent = "#3366FF") {
   ggplot2::theme_bw(
     base_size = base_size,
     base_family = base_family,
+    header_family = header_family,
     base_line_size = base_line_size,
-    base_rect_size = base_rect_size
+    base_rect_size = base_rect_size,
+    ink = ink,
+    paper = paper,
+    accent = accent
   ) +
-    ggplot2::theme(
-      panel.background = ggplot2::element_rect(fill = NA, color = NA),
-      panel.grid.minor = ggplot2::element_blank(),
-      axis.text = ggplot2::element_text(color = "grey20"),
-      axis.ticks = ggplot2::element_line(color = "grey50"),
-      legend.key = ggplot2::element_rect(fill = NA, color = NA)
-    )
+    theme_sub_wtl(ink = ink, paper = paper)
+}
+
+theme_sub_wtl = function(
+    ink = "black", paper = "white",
+    rel = 1.0 / 1.2,
+    continuous = NULL, discrete = NULL) {
+  theme_sub_misc(ink = ink, paper = paper) +
+    theme_sub_text_small(rel = rel) +
+    theme_sub_palette(continuous = continuous, discrete = discrete)
+}
+
+theme_sub_misc = function(ink = "black", paper = "white") {
+  ggplot2::theme(
+    panel.grid.minor = ggplot2::element_blank(),
+    axis.text = ggplot2::element_text(color = scales::col_mix(ink, paper, 0.25)),
+    axis.ticks = ggplot2::element_line(color = scales::col_mix(ink, paper, 0.5)),
+    legend.key = ggplot2::element_rect(fill = NA, color = NA)
+  )
+}
+
+theme_sub_text_small = function(rel = 1.0 / 1.2) {
+  ggplot2::theme(
+    axis.text = ggplot2::element_text(size = ggplot2::rel(rel)),
+    legend.text = ggplot2::element_text(size = ggplot2::rel(rel)),
+    strip.text = ggplot2::element_text(size = ggplot2::rel(rel)),
+    plot.caption = ggplot2::element_text(size = ggplot2::rel(rel))
+  )
+}
+
+theme_sub_palette = function(continuous = NULL, discrete = NULL) {
+  if (is.null(continuous)) continuous = "viridis"
+  if (is.null(discrete)) {
+    discrete = grDevices::palette.colors(palette = "Okabe-Ito")[-1]
+  }
+  ggplot2::theme(
+    palette.colour.continuous = continuous,
+    palette.fill.continuous = continuous,
+    palette.colour.discrete = discrete,
+    palette.fill.discrete = discrete
+  )
 }
 
 #' @description
 #' `erase()` is a shortcut for `theme(... = element_blank())`.
-#' @param ... elements to be blank
-#' @param .names string vector of element names
+#' @param ... The elements to get blank.
+#' @param .names A string vector of element names.
 #' @rdname ggtheme
 #' @export
 erase = function(..., .names = NULL) {
@@ -37,14 +91,15 @@ erase = function(..., .names = NULL) {
 
 #' @description
 #' `axis_line()` sets L-shaped axes.
+#' @param linewidth,linetype Passed to [ggplot2::element_line()].
 #' @inheritParams ggplot2::element_line
 #' @rdname ggtheme
 #' @export
 axis_line = function(colour = NULL, linewidth = NULL, linetype = NULL, lineend = NULL,
-                     color = NULL, arrow = NULL, inherit.blank = FALSE) {
+                     color = NULL, arrow = NULL, arrow.fill = NULL, inherit.blank = FALSE) {
   elem = ggplot2::element_line(
     colour = colour, linewidth = linewidth, linetype = linetype, lineend = lineend,
-    color = color, arrow = arrow, inherit.blank = inherit.blank
+    color = color, arrow = arrow, arrow.fill = arrow.fill, inherit.blank = inherit.blank
   )
   ggplot2::theme(panel.border = ggplot2::element_blank(), axis.line = elem)
 }
