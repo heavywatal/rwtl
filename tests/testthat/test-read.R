048f913e7faeb13c12b220d09f6d3f7354afcfae
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-03-05 14:30:44 +0900

    test: Set readr.show_col_types = FALSE to silence annoying messages

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,4 +1,5 @@
 test_that("read_boost_ini works", {
+  withr::local_options(readr.show_col_types = FALSE)
   ini = "x = 42\ny = text\n"
   tsv = "x\ty\n42\ttext\n"
   expect_identical(read_boost_ini(I(ini)), readr::read_tsv(I(tsv)))
