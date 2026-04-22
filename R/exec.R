886b4da27b41194bb9b0cb4986a0cf635176a7de
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-04-22 15:26:20 +0900

    feat: Set knitr options, message=NA and warning=NA, for safety

--- R/exec.R
+++ R/exec.R
@@ -1,7 +1,14 @@
 #' Shortcut to use knitr and rmarkdown from commandline
 #'
+#' `exec_knit()` temporarily modifies the environment before knitting:
+#' - Set the working directory to the output directory as recommended by knitr.
+#' - Set `unnamed.chunk.label` and `cache.path` using the input file name to avoid conflicts.
+#' - Set `error = FALSE`, `message = NA`, and `warning = NA` for easier debugging.
+#' - Customize `knitr.progress.fun` for better progress display.
+#' - Enable caching for the Stan engine.
 #' @param input File path.
 #' @param output File or directory path.
+#' @returns `exec_knit()` returns the output from `knitr::knit()`.
 #' @rdname exec
 #' @export
 exec_knit = function(input, output = NA) {
@@ -25,12 +32,15 @@ exec_knit = function(input, output = NA) {
   knitr::opts_knit$set(unnamed.chunk.label = glue("_{inputname}"))
   knitr::opts_chunk$set(cache.path = glue(".cache/{inputname}/"))
   knitr::opts_chunk$set(error = FALSE)
+  knitr::opts_chunk$set(message = NA)
+  knitr::opts_chunk$set(warning = NA)
   withr::local_options(knitr.progress.fun = knitr_progress)
   knit_engines_set_cache_stan()
   invisible(knitr::knit(input, output))
 }
 
 #' @param output_dir Directory path.
+#' @returns `exec_render()` returns the output from `rmarkdown::render()`.
 #' @rdname exec
 #' @export
 exec_render = function(input, output_dir = NA) {
